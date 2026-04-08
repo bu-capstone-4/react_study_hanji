de79b5661c0e92d750b983ff7bfc3b6d2dd9a39f
hanjiwon818 <hanjiwon815@naver.com>
2026-04-08 19:16:50 +0900

    todo 검색, 필터탭터 기능 추가
    - 검색어 저장하는 searchQuery 상태 변수 생성
    - TodoSearch 컴포넌트를 만들어 onChange() 이벤트를 통해 검색창에 입력할
      때마다 setSearchQuery 상태 변경 함수를 실행해 저장
    - useDebounce라는 새로운 훅을 만들어 useEffect를 통해 value 값이 변경될
      때마다 setTimeout 실행
      => 새로운 값이 입력될 때마다 clearTimeout이 setTimeout을 중단 시킴
    - todoList에 useDebounce 돌린 값이 포함되는 게 있으면 그 값을 저장하는 filteredTodo 변수 생성
    - filterTodo가 all이면 전체를 보여주기 때문에 filteredTodo 기본값 => return filteredTodo
    - filterTodo가 active면 isDone: false인 값 => return filteredTodo.isDone === false
    - filterTodo가 done이면 isDone: true인 값 => return filteredTodo.isDone === true
    - filteredTodos를 map해서 해당 todo 화면에 출력

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -13,6 +13,9 @@ export interface Todo {
 
 export default function TodoList() {
     const [todoList, setTodoList] = useState<Todo[]>([]); //할일 추가했을 때 저장되는 배열
+    const [searchQuery, setSearchQuery] = useState(''); //검색어 저장
+    const debouncedQuery = useDebounce(searchQuery);
+    const [filterTodo, setFilterTodo] = useState<'all' | 'active' | 'done'>('all'); //<'all'|'active'|'done'> 이 3개만 들어올 수 있다는 표시, ('all') 기본값
 
     const addTodo = (inputText: string) => {
         const newTodo: Todo = {
@@ -37,15 +40,70 @@ export default function TodoList() {
             }
         }));
     }
+    // filteredTodo: 검색했을 때 나오는 값, 아무것도 검색 안 하면 전체 보여줌
+    // filterTodo: 상태를 가리킴 "all", "active", "done"
+    // isDone: true: 완료, false: 미완료
+    // => 검색한 내용이 기본값이고 
+    // filterTodo가 all이면 전체를 보여주기 때문에 filteredTodo 기본값 => return filteredTodo
+    // filterTodo가 active면 isDone: false인 값 => return filteredTodo.isDone === false
+    // filterTodo가 done이면 isDone: true인 값 => return filteredTodo.isDone === true
+
+    const filteredTodos = useMemo(() => {
+        const filteredTodo = todoList.filter((todo) => {
+            return todo.text.includes(debouncedQuery);
+        });
+        if (filterTodo === 'all') {
+            return filteredTodo;
+        } else if (filterTodo === 'active') {
+            return filteredTodo.filter((todo) => {
+                return todo.isDone === false;
+            })
+        } else {
+            return filteredTodo.filter((todo) => {
+                return todo.isDone === true;
+            })
+        }
+    }, [todoList, filterTodo, debouncedQuery])
+
 
 
     return (
         <div>
             <TodoInput addTodo={addTodo}></TodoInput>
-            {todoList.map((todo)=> {
-                return <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo}></TodoItem> 
+            <TodoSearch
+                setSearchQuery={setSearchQuery}
+            ></TodoSearch>
+            <button
+                className={filterTodo === 'all' ? 'bg-blue-400' : 'bg-gray-300'}
+                onClick={() => {
+                    setFilterTodo('all');
+                }}>
+                전체
+            </button>
+            <button
+                className={filterTodo === 'active' ? 'bg-blue-400' : 'bg-gray-300'}
+                onClick={() => {
+                    setFilterTodo('active');
+                }}>
+                미완료
+            </button>
+            <button
+                className={filterTodo === 'done' ? 'bg-blue-400' : 'bg-gray-300'}
+                onClick={() => {
+                    setFilterTodo('done');
+                }}>
+                완료
+            </button>
+            {/* {todoList.filter((todo) => { //{} return 생략 불가능
+                return todo.text.includes(debouncedQuery)
+            }).map((todo) => ( //() return 생략 가능
+                <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo}></TodoItem>
+            ))
+            } */}
+            {filteredTodos.map((todo) => {
+                return <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo}></TodoItem>
             })}
-           
+
         </div>
     )
 }
